f43471b3c7b51d4d7f4418e0278f4e21ff4ef179
coolestcoder655 <74312880+coolestcoder655@users.noreply.github.com>
2025-05-01 08:35:29 -0500

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -174,16 +174,11 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
-  "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
-  "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "Arnik is very gay",
+  "I'm super cool",
+  "I know.",
+  "The FitnessGram Pacer Test is a multistage aerobic capacity test...",
+  "Arnik, don't report me :3"
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);

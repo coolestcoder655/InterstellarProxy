88c47796e6f01417bdb4a6f0135949bca926393f
Maz Khokhar <74312880+coolestcoder655@users.noreply.github.com>
2025-05-07 15:06:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
     // You can add multiple users by doing username: 'password'.
     nerd: "isNotSmart",  // Me
     LittleTim2359: "Incineroar#737",  // Timothy
-    chingChong: "scammerMan",  // Arnik
+    // chingChong: "scammerMan",  // Arnik
     Skibidiaura2k: "1553pineapple",  // Walker
     // arab: "terroism"  // Rayaan
     // thesea: "theDeap", // Khalil
